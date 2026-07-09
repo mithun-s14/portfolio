64362046013b117dd84b58e637fc310d3b15fbe2
Mithun Sivapathasundram <mithunsivapathasundram@outlook.com>
2026-07-08 23:54:48 -0400

    fix: font update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,5 @@
 import type { Metadata } from "next";
-import { Inter, Big_Shoulders } from "next/font/google";
 import "./globals.css";
-
-const inter = Inter({
-  variable: "--font-inter",
-  subsets: ["latin"],
-  display: "swap",
-});
-
-const bigShoulders = Big_Shoulders({
-  variable: "--font-big-shoulders",
-  subsets: ["latin"],
-  display: "swap",
-  weight: "variable",
-});
 
 export const metadata: Metadata = {
   title: "Mithun Sivapathasundram",
@@ -31,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.variable} ${bigShoulders.variable} overflow-x-hidden`}>{children}</body>
+      <body className="overflow-x-hidden">{children}</body>
     </html>
   );
 }
